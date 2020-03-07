65e49e340a1fad4280f6fcaeb9d9b4a60cf522d7
Dmitry <it.portretnyy@gmail.com>
2020-03-07 21:46:41 +0300

    Added: new education classes.

---
+++ main/src/arrays/ArraysDemo.java
@@ -0,0 +1,57 @@
+package arrays;
+
+public class ArraysDemo {
+
+    public static void main(String[] args) {
+        int [] x;
+        int y[];//bad style
+        int[] numbers = {1, 2, 3, 4, 5};//array of 4 numbers
+        int[] otherNumbers = new int[6];//array of 6 numbers
+        String[] strings = new String[3];//array of 3 null String
+        String[] otherStrings = {"test", "test3", "hello"}; //array of 3 nonNull strings
+//        printArray(new int[]{1,2,3,11,40});
+//        System.out.println();
+//        printArray(new String[]{"test", "123"});
+
+//        System.out.println("numbers[5] = " + numbers[5]);//error!
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        int i = 0;//bad codeStyle
+        while (i < numbers.length) {
+            System.out.println("* " + numbers[i]);
+            i++;
+        }
+
+        for (int num : numbers) {
+            //num === numbers[i]
+            System.out.println("?" + num);
+        }
+
+        for (int number : numbers) {
+
+        }
+
+        for (int j = 0; j < numbers.length; j++) {
+
+        }
+
+        for (int k = numbers.length - 1; k >= 0; k--) {
+
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + "; ");
+        }
+        System.out.println();
+    }
+
+    public static void printArray(String[] arr) {
+        for (String s : arr) {
+            System.out.print(s + ", ");
+        }
+    }
+}
---
+++ main/src/arrays/SortingDemo.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class SortingDemo {
+    public static void main(String[] args) {
+        int[] arr = {78, 77, 46, 12, 3 ,6, -1};
+        //after sorting-> arr == {-1,2,3,10,42,78}
+//        ArraysDemo.printArray(arr);
+//        int temp = arr[0];
+//        arr[0] = arr[1];
+//        arr[1] = temp;
+        ArraysDemo.printArray(arr);
+        System.out.println("++++++++++++");
+        //n(n-1) operations
+        //could be enhanced to n(n-1)/2
+        for (int j = 0; j < arr.length; j++) {
+            for (int i = 0; i < arr.length-1; i++) {
+                if (arr[i] > arr[i+1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+                ArraysDemo.printArray(arr);
+            }
+            System.out.println("-------------------------");
+        }
+
+
+    }
+}
