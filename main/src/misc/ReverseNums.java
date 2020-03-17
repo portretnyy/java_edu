15621b65072dc6fbea03c4bfa909294f62a1f2fb
Dmitry <it.portretnyy@gmail.com>
2020-03-17 11:38:21 +0300

    Added: a solution for the ReverseNums class;

--- main/src/misc/ReverseNums.java
+++ main/src/misc/ReverseNums.java
@@ -4,7 +4,20 @@ public class ReverseNums {
 
 
     //{1,4,-1,6} -> {6,-1,4,1}
-    public static void reverse(int[] arr) {
+    public static int reverse(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            if (arr[i] != arr[arr.length - i - 1]) {
+                System.out.println("This array is not a palindrome.");
+                return 0;
+            }
+        }
+        System.out.println("This array is a palindrome.");
+        return 0;
+    }
+    // Решение можно реализовать и с помощью void метода, просто тренируюсь расставлять оператор return
 
+    public static void main(String[] args) {
+        int[] x = {1,1,3,3,1,1,};
+        reverse(x);
     }
 }
