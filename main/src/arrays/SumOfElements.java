0c8b778fc1d44f20bf04bc4e3ccd57c45a0613e8
portretnyy <it.portretnyy@gmail.com>
2020-03-23 19:49:49 +0300

    Added: solutions for SumOfElements and Problem_26 classes.

--- main/src/arrays/SumOfElements.java
+++ main/src/arrays/SumOfElements.java
@@ -15,7 +15,18 @@ public class SumOfElements {
 
  */
 
+    public static int sum(int arr[], int x) {
+        int sum = 0;
+        for (int i = 1; i <= x; i++) {
+            sum += arr[i - 1];
+        }
+        return sum;
+    }
+
     public static int[] sumOfElements(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = sum(arr, i);
+        }
         return arr;
     }
 
--- main/src/leetCode/Problem_26.java
+++ main/src/leetCode/Problem_26.java
@@ -6,29 +6,17 @@ public class Problem_26 {
 
     public static int removeDuplicates (int arr[]) {
         //[1,2,2,4,15,15,16]->[1,2,4,15,16,0,0]
-        int result = 0;
-        if (arr.length == 1) {
-            result = 1;
-            return result;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 2; j++) {
-                if (arr[j] == arr[j + 1]) {
-                    arr[j + 1] = arr[j + 2];
-                }
+        int index = 0;
+        int current = arr[0];
+        for (int i = 0; i < arr.length; i ++) {
+            if (arr[i] != current) {
+                index += 1;
+                current = arr[i];
+                arr[index] = current;
             }
         }
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] == arr[i + 1]) {
-                result = i + 1;
-                ArraysDemo.printArray(arr);
-                return result;
-            }
-        }
-        return 0;
+        return index + 1;
     }
-
-
 
     public static void main(String[] args) {
         int[] array = {1,2};
