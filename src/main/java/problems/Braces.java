cb342f51949c7e0d9ed9a27299fd46586168ec2b
Рахманкулов Эдуард <Rakhmankulov.E.R@sberbank.ru>
2020-04-09 15:06:36 +0300

    задача по скобкам

---
+++ src/main/java/problems/Braces.java
@@ -0,0 +1,18 @@
+package problems;
+
+
+/**
+ * задачи с на проверку валидности скобок
+ */
+public class Braces {
+
+    /**
+     * входная строка должна состоять из двух видов символов '(' и ')'
+     *
+     * @param input входная строка
+     * @throws IllegalArgumentException если присутствуют иные символы
+     */
+    public static boolean isValidOneType(String input) throws IllegalArgumentException {
+        return false;
+    }
+}
---
+++ src/test/java/problems/BracesTest.java
@@ -0,0 +1,48 @@
+package problems;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+import static problems.Braces.isValidOneType;
+
+
+public class BracesTest {
+
+    private static final String REPEATED_BRACES = ")" + "()".repeat(500000);
+
+    @Test
+    public void testOneType() {
+        assertTrue(isValidOneType("(())"));
+        assertTrue(isValidOneType("()()"));
+        assertTrue(isValidOneType("()()"));
+        assertTrue(isValidOneType("()()()()()()"));
+        assertTrue(isValidOneType("()".repeat(100)));
+        assertTrue(isValidOneType("(((((())))))"));
+        assertTrue(isValidOneType("(())"));
+        assertTrue(isValidOneType(""));
+
+        assertFalse(isValidOneType(null));
+        assertFalse(isValidOneType("(()))"));
+        assertFalse(isValidOneType(")))"));
+        assertFalse(isValidOneType("("));
+        assertFalse(isValidOneType("(((((("));
+        assertFalse(isValidOneType("(".repeat(100) + ")".repeat(99)));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testOneTypeShouldThrowException() {
+        isValidOneType("(test)");
+    }
+
+    @Test
+    public void testOneTypeSpeed() {
+        final long start = System.nanoTime();
+
+        isValidOneType(REPEATED_BRACES);
+
+        final long end = System.nanoTime();
+        System.out.println(end - start);
+        assertTrue(end - start < 10_000_000);
+    }
+}
