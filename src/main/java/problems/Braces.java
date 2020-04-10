9ec84a94a57be86059118f5d10ce8fb9b72f9752
Dmitry <it.portretnyy@gmail.com>
2020-04-11 00:07:20 +0300

    Added: Braces solution

--- src/main/java/problems/Braces.java
+++ src/main/java/problems/Braces.java
@@ -1,6 +1,8 @@
 package problems;
 
 
+import java.util.*;
+
 /**
  * задачи с на проверку валидности скобок
  */
@@ -13,6 +15,31 @@ public class Braces {
      * @throws IllegalArgumentException если присутствуют иные символы
      */
     public static boolean isValidOneType(String input) throws IllegalArgumentException {
-        return false;
+
+        if (input == null || input.length() % 2 != 0) {
+            return false;
+        }
+
+        Deque<Character> braces = new LinkedList<>();
+        char currentChar;
+        for (int i = 0; i < input.length(); i++) {
+            currentChar = input.charAt(i);
+            if (currentChar != '(' && currentChar != ')') {
+                throw new IllegalArgumentException("Wrong string!");
+            } else braces.push(currentChar);
+        }
+
+        for (int i = 0; i < braces.size() / 2; i++) {
+            if (braces.peekLast() == braces.peekFirst()) return false;
+            else {
+                braces.pollFirst();
+                braces.pollLast();
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        isValidOneType("((()))");
     }
 }
